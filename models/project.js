56290a1323596d42fa663fe84384e4504b6e2d6e
Thierry Bitz <bitzthierry@gmail.com>
2022-04-24 17:12:03 +0000

    Feature/filter by projects

--- models/project.js
+++ models/project.js
@@ -7,6 +7,7 @@ const projectSchema = new mongoose.Schema({
   name: { type: String, required: true },
   user: { type: ObjectId, ref: "user", required: true },
   description: { type: String },
+  kwh: { type: String },
   tasks: { type: Array, default: [] },
   img: { type: String },
 });
@@ -16,6 +17,7 @@ const validate = (project) => {
     name: Joi.string().required(),
     user: Joi.string().required(),
     description: Joi.string().allow(""),
+    kwh: Joi.string().required(),
     tasks: Joi.array().items(Joi.string()),
     img: Joi.string().allow(""),
   });
