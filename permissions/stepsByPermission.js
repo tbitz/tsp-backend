659ea5c74c22917d4e17c617d4c162193bb102c5
Thierry Bitz <bitzthierry@gmail.com>
2022-06-01 23:14:48 +0200

    new roles and correct filtering

--- permissions/stepsByPermission.js
+++ permissions/stepsByPermission.js
@@ -1,21 +1,25 @@
 function filteredSteps(req, steps) {
-  if (req.user.role === "Admin") {
+  if (req.user.role === "Admin" || req.user.role === "Projektleiter") {
     return steps;
   }
-  if (req.user.role === "Monteur") {
+  if (req.user.role === "Verkauf") {
+    return steps.filter((step) => ["Projektstart"].includes(step.name));
+  }
+  if (req.user.role === "Chef-Monteur") {
     return steps.filter((step) => ["Montage"].includes(step.name));
   }
-
-  if (req.user.role === "Projektleiter") {
+  if (req.user.role === "Service-Monteur") {
     return steps.filter((step) =>
-      [
-        "Projektstart",
-        "Projektplanung",
-        "Beschaffungen",
-        "Letzte Vorbereitungen",
-        "Projektabschluss",
-      ].includes(step.name)
+      ["Montage", "Inbetriebnahme"].includes(step.name)
     );
+  }
+  if (req.user.role === "Sachbearbeiter Finanz und Controlling") {
+    return steps.filter((step) =>
+      ["Projektplanung", "Projektabschluss"].includes(step.name)
+    );
+  }
+  if (req.user.role === "Lagerist") {
+    return steps.filter((step) => ["Beschaffung"].includes(step.name));
   }
 }
 
--- permissions/tasksByPermission.js
+++ permissions/tasksByPermission.js
@@ -2,33 +2,40 @@
  * supported roles: Admin, Monteur, Projektleiter
  * toString is changing ObjectId("id") to "id"
  */
+const filterAllowedSteps = (steps, tasks, allowedSteps) => {
+  const filteredStepIds = steps
+    .filter((step) => allowedSteps.includes(step.name))
+    .map((step) => step._id.toString());
+  return tasks.filter((task) => filteredStepIds.includes(task.stepId));
+};
+
 function filteredTasks(req, steps, tasks) {
   if (req.user.role === "Admin") {
     return tasks;
   }
-  if (req.user.role === "Monteur") {
-    const filteredStepIds = steps
-      .filter((step) => ["Montage"].includes(step.name))
-      .map((step) => step._id.toString());
+  if (req.user.role === "Verkauf")
+    return filterAllowedSteps(steps, tasks, ["Projektstart"]);
+  if (req.user.role === "Chef-Monteur")
+    return filterAllowedSteps(steps, tasks, ["Montage"]);
+  if (req.user.role === "Service-Monteur")
+    return filterAllowedSteps(steps, tasks, ["Montage", "Inbetriebnahme"]);
+  if (req.user.role === "Sachbearbeiter Finanz und Controlling")
+    return filterAllowedSteps(steps, tasks, [
+      "Projektplanung",
+      "Projektabschluss",
+    ]);
+  if (req.user.role === "Lagerist")
+    return filterAllowedSteps(steps, tasks, ["Beschaffung"]);
+  if (req.user.role === "Projektleiter")
+    return filterAllowedSteps(steps, tasks, [
+      "Projektstart",
+      "Projektplanung",
+      "Beschaffung",
+      "Letzte Vorbereitungen",
+      "Projektabschluss",
+    ]);
 
-    return tasks.filter((task) => filteredStepIds.includes(task.stepId));
-  }
-
-  if (req.user.role === "Projektleiter") {
-    const filteredStepIds = steps
-      .filter((step) =>
-        [
-          "Projektstart",
-          "Projektplanung",
-          "Beschaffungen",
-          "Letzte Vorbereitungen",
-          "Projektabschluss",
-        ].includes(step.name)
-      )
-      .map((step) => step._id.toString());
-
-    return tasks.filter((task) => filteredStepIds.includes(task.stepId));
-  }
+  return tasks.filter((task) => filteredStepIds.includes(task.stepId));
 }
 
 module.exports = {
