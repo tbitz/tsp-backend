c567e66ff1e58b5bfbcbd8c7b58c0e64ab53ed0d
Thierry Bitz <bitzthierry@gmail.com>
2022-06-02 16:36:02 +0200

    timeline with project and step filtering

--- models/column.js
+++ models/column.js
@@ -5,14 +5,14 @@ const ObjectId = mongoose.Schema.Types.ObjectId;
 const columnSchema = new mongoose.Schema({
   user: { type: String, ref: "user", required: true },
   title: { type: String, required: true },
-  stepId: { type: String, required: true },
+  index: { type: Number, required: true },
 });
 
 const validate = (column) => {
   const schema = Joi.object({
     user: Joi.string().required(),
     title: Joi.string().required(),
-    stepId: Joi.string().required(),
+    index: Joi.number().required(),
   });
   return schema.validate(column);
 };
