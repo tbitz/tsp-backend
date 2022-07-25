57a21435cafe7331842c672bfe9a1707689eaefb
Thierry Bitz <bitzthierry@gmail.com>
2022-07-25 15:16:18 +0200

    steps adapted

--- models/step.js
+++ models/step.js
@@ -3,10 +3,21 @@ const Joi = require("joi");
 
 const stepSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  step: { type: String, required: true },
+  stepIndex: { type: String, required: true },
   categoryId: { type: String, required: true },
+  prefillTasks: { type: [Object], default: [] },
 });
+
+const validate = (category) => {
+  const schema = Joi.object({
+    name: Joi.string().required(),
+    stepIndex: Joi.string().required(),
+    categoryId: Joi.string().required(),
+    prefillTasks: Joi.array().items(Joi.object()),
+  });
+  return schema.validate(category);
+};
 
 const Step = mongoose.model("step", stepSchema);
 
-module.exports = { Step };
+module.exports = { Step, validate };
